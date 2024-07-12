4cdb2f42fbc8abf9e082a4da54e8e444c9fbecdb
Aaron Urrea <aaronurrea343@gmail.com>
2024-07-12 15:29:07 -0700

    Clear and Press logic complete

--- javascript.js
+++ javascript.js
@@ -1,11 +1,55 @@
-function operate(array){
+const BUTTONS = document.querySelectorAll("button");
+const HISTORY = document.querySelectorAll(".display .row#history");
+const INPUT = document.querySelector(".display .row#input");
+
+let input = document.querySelector(".row#input").querySelector("#equation");
+
+BUTTONS.forEach((button) => {
+    button.addEventListener("click", function(){
+        press(button.textContent);
+    });
+})
+
+function clear(){
+    input.textContent = '';
+}
+
+function operate(){
 
 }
 
-function parse(string){
-
+function appendInput(pressedButton){
+    if(input.textContent.length < 25)
+        input.textContent += pressedButton;
 }
 
 function add(...args){
 
+}
+
+function subtract(...args){
+
+}
+
+function mutliply(...args){
+    
+}
+
+function divide(...args){
+
+}
+
+
+function press(pressedButton){
+    switch(pressedButton){
+        case 'C':
+            clear();
+            break;
+        case '=':
+            operate();
+            break;
+        default:
+            appendInput(pressedButton);
+            break;
+    }
 }
